0f9a6145e290c4621917afe9eeeda371b4f213a2
吉崎祐介 <yoshizakiyusuke@yoshizakiyuusukenoMacBook-Air.local>
2023-12-11 23:32:08 +0900

    Rubocopの実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze # rubocop:disable Style/RedundantFreeze
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
