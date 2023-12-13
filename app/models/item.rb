f5f57ad831df34ee8892a1c38e772852d7758c53
吉崎祐介 <yoshizakiyusuke@yoshizakiyuusukenoMacBook-Air.local>
2023-12-13 18:09:12 +0900

    ActiveHash(Prefecture)の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,7 @@ class Item < ApplicationRecord
   belongs_to :category
   belongs_to :condition
   belongs_to :delivery_charge
+  belongs_to :prefecture
   belongs_to :user
   has_one_attached :image
 end
