cc44a2644a17013c3e819604b65daa4687b6acec
吉崎祐介 <yoshizakiyusuke@yoshizakiyuusukenoMacBook-Air.local>
2023-12-16 21:43:26 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,4 +7,12 @@ class Item < ApplicationRecord
   belongs_to :shipping_time
   belongs_to :user
   has_one_attached :image
+
+  validates :image, presence: true
+  validates :name, presence: true, length: { maximum: 40 }
+  validates :content, presence: true, length: { maximum: 1000 }
+  validates :category_id, :condition_id, :delivery_charge_id, :prefecture_id, :shipping_time_id,
+            numericality: { other_than: 1, message: "can't be blank" }
+  validates :price, presence: true, numericality: { in: 300..9_999_999 }
+  validates_format_of :price, with: /\A\d+\z/, message: 'は半角数字で入力してください'
 end
