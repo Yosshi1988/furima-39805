85cc73219cd5386ad48a5121268ac119e93a024a
吉崎祐介 <yoshizakiyusuke@yoshizakiyuusukenoMacBook-Air.local>
2023-12-24 08:03:13 +0900

    itemsコントローラーの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
+  before_action :set_item, only: [:show, :edit, :update]
   before_action :move_to_index, only: :edit
 
   def index
@@ -20,15 +21,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item.id)
     else
@@ -36,15 +34,18 @@ class ItemsController < ApplicationController
     end
   end
 
-  def move_to_index
-    item = Item.find(params[:id])
-    redirect_to root_path unless current_user.id == item.user_id
-  end
-
   private
 
   def item_params
     params.require(:item).permit(:name, :content, :category_id, :condition_id, :delivery_charge_id, :prefecture_id,
                                  :shipping_time_id, :price, :image).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def move_to_index
+    redirect_to root_path unless current_user.id == @item.user_id
   end
 end
