d696d5c69cad4b7dc552c94f1123ba315043ebd2
吉崎祐介 <yoshizakiyusuke@yoshizakiyuusukenoMacBook-Air.local>
2024-01-19 06:00:49 +0900

    itemモデルのエラーメッセージ日本語化

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,7 +13,7 @@ class Item < ApplicationRecord
   validates :name, presence: true, length: { maximum: 40 }
   validates :content, presence: true, length: { maximum: 1000 }
   validates :category_id, :condition_id, :delivery_charge_id, :prefecture_id, :shipping_time_id,
-            numericality: { other_than: 1, message: "can't be blank" }
+            numericality: { other_than: 1, message: 'を選択してください' }
   validates :price, presence: true,
                     numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 end
