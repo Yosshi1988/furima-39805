f5093d9b21ef2409b5b708bbb19fff11d1f21e7a
吉崎祐介 <yoshizakiyusuke@yoshizakiyuusukenoMacBook-Air.local>
2024-01-20 07:58:13 +0900

    purchase_addressモデルのエラーメッセージ日本語化

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -5,12 +5,12 @@ class PurchaseAddress
   with_options presence: true do
     validates :item_id
     validates :user_id
-    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
-    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'はハイフン(-)を含む半角数字で入力してください' }
+    validates :prefecture_id, numericality: { other_than: 1, message: 'を選択してください' }
     validates :city
     validates :street_address
     validates :tel,
-              format: { with: /\A[0-9]{10,11}\z/, message: 'is half-width number. Telephone number should be 10 to 11 digits' }
+              format: { with: /\A[0-9]{10,11}\z/, message: 'は10桁または11桁の半角数字で入力してください' }
     validates :token
   end
 
